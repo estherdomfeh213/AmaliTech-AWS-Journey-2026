b1a60ff7376f2c029cfdfd391363883ad1bf59a6
estherdomfeh213 <domfehesther2020@gmail.com>
2026-03-11 17:35:53 +0000

    doc: update readme

--- ec2-rds-connection/scripts/database-setup.sql
+++ ec2-rds-connection/scripts/database-setup.sql
@@ -1,31 +1,31 @@
-# Database Setup 
+-- Database Setup 
 
-## Create Database
-```sql 
+--Create Database
+
 CREATE DATABASE SchoolDB;
-```
-Verify creation:
 
-```bash
+--Verify creation:
 SHOW DATABASES;
-```
 
-## Create Table 
-```sql
+
+-- Create Table 
+
 USE SchoolDB ;
-CREATE TABLE IF NOT EXISTS subjects (subject_id INT AUTO_INCREMENT,subject_name VARCHAR(255) NOT NULL,teacher VARCHAR(255),start_date DATE,lesson TEXT,PRIMARY KEY (subject_id)) ENGINE=INNODB;
-```
 
-## Insert some details into the table:**
-```sql
+
+CREATE TABLE IF NOT EXISTS subjects (subject_id INT AUTO_INCREMENT,subject_name VARCHAR(255) NOT NULL,teacher VARCHAR(255),start_date DATE,lesson TEXT,PRIMARY KEY (subject_id)) ENGINE=INNODB;
+
+-- Insert some details into the table:**
+
 INSERT INTO subjects(subject_name, teacher) VALUES ('English', 'John Taylor');
 INSERT INTO subjects(subject_name, teacher) VALUES ('Science', 'Mary Smith');
 INSERT INTO subjects(subject_name, teacher) VALUES ('Maths', 'Ted Miller');
 INSERT INTO subjects(subject_name, teacher) VALUES ('Arts', 'Suzan Carpenter');
 ```
 
-## Let's check the items we added into the table:
-```sql
-select * from subjects;
-```
+--Let's check the items we added into the table:
 
+select * from subjects;
+
+
+exit 
